3dbc1b5e5b6d8774a9dcc4d2fffa3200a99a6d6c
menneg_l <menneg_l@etna-alternance.net>
2023-06-20 13:09:57 +0200

    create file

--- test.php
+++ test.php
@@ -1,2 +1,7 @@
 <?php
-echo "test";
+$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+$txt = "John Doe\n";
+fwrite($myfile, $txt);
+$txt = "Jane Doe\n";
+fwrite($myfile, $txt);
+fclose($myfile);
